4d7ea07ff0ba09bae867b593ea6916376cabd826
Sagi Buria <sagib313@gmail.com>
2021-07-18 13:01:55 +0300

    New styles and images

--- Client.py
+++ Client.py
@@ -2,14 +2,16 @@ import pickle
 import socket
 import threading
 import tkinter
+from tkinter.constants import RIGHT
 import tkinter.scrolledtext
 from tkinter import simpledialog
-from typing import Tuple
 from Crypto.Cipher import AES
 import rsa
 
 host_ip = socket.gethostname()    # My local IP adress
 host_port = 5555
+
+
 
 class Client:
     def __init__(self,hostIP,port):
@@ -22,8 +24,11 @@ class Client:
         self.sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         self.sock.connect((hostIP,port))
 
+
         self.gui_done = False
         self.running = True
+        
+        msg.destroy()
 
         gui_thread= threading.Thread(target=self.gui_loop)
         receive_thread= threading.Thread(target=self.receive)
@@ -32,43 +37,47 @@ class Client:
         receive_thread.start()
 
     def gui_loop(self):
-        self.win=tkinter.Tk()
-        self.win.configure()
+        self.window=tkinter.Tk()
+        self.window.title("Secure Chat Room")
+        self.window.resizable(width=False, height=False)
+        self.window.configure()
 
-        self.chat_label= tkinter.Label(self.win,text="chat")
+        self.chat_label = tkinter.Label(self.window,text="Chat Room", font=("Times", 30, "bold italic"))
         self.chat_label.pack(padx=20,pady=5)
 
-        self.text_area=tkinter.scrolledtext.ScrolledText(self.win)
+        self.text_area=tkinter.scrolledtext.ScrolledText(self.window)
         self.text_area.pack(padx=20,pady=5)
         self.text_area.config(state='disabled') # So the user won't be able to change the chat history
-
-
         
-        self.input_area=tkinter.Text(self.win,height=3)
+        self.input_area = tkinter.Text(self.window,height=3)
         self.input_area.pack(padx=20,pady=5)
-
-        self.send_button= tkinter.Button(self.win, text="send",command=self.write)
+        
+        self.button_img = tkinter.PhotoImage(file="./Images/Button.gif")
+        self.send_button = tkinter.Button(self.window, text="send", command = self.write, image = self.button_img, highlightthickness = 0, bd = 0)
         self.send_button.pack()
 
+        self.chat_label = tkinter.Label(self.window,text="@Made by- Sagi Buria", font=("Times", 8))
+        self.chat_label.pack(anchor='w')
 
         self.gui_done=True
-        self.win.mainloop()
+        self.window.mainloop()
 
     def write(self):
-        message = f"{self.nickname}: {self.input_area.get('1.0','end')}"    # Create the message template like this- "Nickname: text"
-        
-        cipher = AES.new(self.symetric_key, AES.MODE_EAX)   # Encrypt the sent message
-        nonce = cipher.nonce
-        ciphertext, tag = cipher.encrypt_and_digest(message.encode('utf-8'))
+        if len(self.input_area.get('1.0','end')) > 1 :  # To prevent empty messages
+            message = f"{self.nickname}: {self.input_area.get('1.0','end')}"    # Create the message template like this- "Nickname: text"
+            
+            cipher = AES.new(self.symetric_key, AES.MODE_EAX)   # Encrypt the sent message
+            nonce = cipher.nonce
+            ciphertext, tag = cipher.encrypt_and_digest(message.encode('utf-8'))
 
-        tupled_data = pickle.dumps((ciphertext,nonce,tag))  #  The tuple is all the data that is needed for the decryption. It is converted to Bytes to be sent
-        self.sock.send(tupled_data)
+            tupled_data = pickle.dumps((ciphertext,nonce,tag))  #  The tuple is all the data that is needed for the decryption. It is converted to Bytes to be sent
+            self.sock.send(tupled_data)
 
-        self.input_area.delete('1.0','end') # Clears the input field
+            self.input_area.delete('1.0','end') # Clears the input field
     
     def stop(self):
         self.running=False
-        self.win.destroy()
+        self.window.destroy()
         self.sock.close()
         exit(0)
 
@@ -97,10 +106,8 @@ class Client:
                     plaintext = cipher.decrypt(message[0])
                     try:
                         cipher.verify(message[2])
-                        print("The message is authentic:", plaintext)
                     except ValueError:
-                        print("Key incorrect or message corrupted")
-
+                        pass
                     if self.gui_done:
                         self.text_area.config(state='normal')
                         self.text_area.insert('end',plaintext)
